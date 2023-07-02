8c379d7b4f3e51078aca426939ecd5a01ee03754
DanRab8 <136909378+DanRab8@users.noreply.github.com>
2023-07-02 20:38:50 +0000

    customizing one more thing

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "a picture of a yellow and grey crossroad";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a student who enjoys learning new things and discussing them with others.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
